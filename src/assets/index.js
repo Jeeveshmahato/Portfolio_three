4e38c6bccf52d738691c6659e868efe9bf240f1a
Jeeveshmahato <93731220+Jeeveshmahato@users.noreply.github.com>
2024-02-13 01:43:47 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import deploy from './deploy.jpg'
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+// import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+// import mongodb from "./tech/mongodb.png";
+// import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+// import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import cobol from './tech/cobol.jpg';
+import wordpress from './tech/wordpress.png';
+import canva from './tech/canva.png'
+import nextjs from './tech/nextjs.webp'
+
+import ricoz from './company/Ricoz.jpg';
+import cognizant from './company/Cognizant.png'
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import kunal from './kunal.jpeg';
+import rajesh from './rajesh.jpeg';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  deploy,
+  html,
+  javascript,
+ 
+  reactjs,
+  redux,
+  tailwind,
+ 
+  threejs,
+  carrent,
+  jobit,
+  tripguide,
+  ricoz,
+  cognizant,
+  cobol,
+  wordpress,
+  canva,
+  nextjs,
+  kunal,
+  rajesh,
+};
