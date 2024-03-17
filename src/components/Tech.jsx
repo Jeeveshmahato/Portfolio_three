3556d35c51ae9dd58f9c02d53e4b066b6b6fec3f
Jeeveshmahato <93731220+Jeeveshmahato@users.noreply.github.com>
2024-03-17 21:04:03 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../Constants';
+import { BallCanvas } from './canvas';
 
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28 flex gap-2 items-center justify-center' key={technology.name}>
-          {/* Replace BallCanvas with your desired element or component */}
-          <div className=' flex flex-col gap-2 items-center justify-center'>
+          <div className=' hidden xl:flex  flex-col   items-center'>
+            <BallCanvas icon={technology.icon} />
+            
+          </div>
+          <div className=' flex xl:hidden flex-col gap-2 items-center justify-center'>
             <img src={technology.icon} alt={technology.name} />
             <p className=' text-[14px] font-inter  md:text-[18px] font-[500]'>{technology.name}</p>
           </div>
