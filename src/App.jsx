a81ada4521349f3f3ddb34853d50a7f8a3167d2c
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-12-22 21:51:09 +0530

    Repair_navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
-import NewNavbar from './components/Newnavbar'
 const App = () => {
   return (
     <BrowserRouter>
       <div className=' relative z-0 bg-primary'>
         <div className=' bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <NewNavbar />
+          <Navbar />
           <Hero />
 
         </div>
