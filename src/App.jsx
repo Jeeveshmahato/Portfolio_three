4b72e8f556f7281b80d1d12e08d576ae8246e472
Jeeveshmahato <jeeveshmahato100@gmail.com>
2024-12-22 21:04:44 +0530

    Header_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import NewNavbar from './components/Newnavbar'
 const App = () => {
   return (
     <BrowserRouter>
       <div className=' relative z-0 bg-primary'>
         <div className=' bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
+          <NewNavbar />
           <Hero />
 
         </div>
